570ee85142d4375242ba29e1e87a2994f2668a28
Ezalor <gggghhhh2924@163.com>
2016-10-18 13:45:10 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# 修改 bg_apps_limit 限制
+将后台应用限制修改为 42。自用于一加 3 氢 OS。
+
+将 build.prop 修改为：
+
+ro.sys.fw.bg_apps_limit=42
---
+++ common/post-fs.sh
@@ -0,0 +1,6 @@
+#!/system/bin/sh
+
+# This script will be executed in post-fs mode
+# More info in the main Magisk thread
+
+setprop ro.sys.fw.bg_apps_limit 42;
