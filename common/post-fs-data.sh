f580ba1b0dc60addbee5059cb1a3697f042bf421
Ezalor <gggghhhh2924@163.com>
2016-11-30 01:35:55 +0800

    fix

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -3,8 +3,7 @@
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-/data/magisk/resetprop persist.camera.eis.enable 1
-/data/magisk/resetprop persist.camera.is_type 4
+/data/magisk/resetprop ro.sys.fw.bg_apps_limit 42
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
